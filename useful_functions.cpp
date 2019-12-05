844bc0e1fa947348d30681b895794eba9cd4bd74
Vineeth <vineeththota1315268@gmail.com>
2019-12-05 23:26:11 +0530

    Added build segment tree

--- useful_functions.cpp
+++ useful_functions.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
 int gcd(int a,int b){return b?gcd(b,a%b):a;}
+
+void build_segement_tree(vector<int> &a, vector<int> &tree, int start, int end, int node) {
+    if (start == end) {
+        tree[node] = a[start];
+    } else {
+        int mid = (start + end) / 2;
+        build_segement_tree(a, tree, start, mid, 2 * node + 1);
+        build_segement_tree(a, tree, mid + 1, end, 2 * node + 2);
+        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+    }
+}
