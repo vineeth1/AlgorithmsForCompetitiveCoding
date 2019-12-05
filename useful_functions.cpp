ede5d805257b6b39825514899986bbc7e49b2851
Vineeth <vineeththota1315268@gmail.com>
2019-12-05 23:53:39 +0530

    Segment tree done

--- useful_functions.cpp
+++ useful_functions.cpp
@@ -12,3 +12,31 @@ void build_segement_tree(vector<int> &a, vector<int> &tree, int start, int end, 
         tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
     }
 }
+
+void segment_tree_update(vector<int> &a, vector<int> &tree, int start, int end, int node, int i, int value) {
+    if (start == end) {
+        tree[node] = value;
+        a[start] = value;
+    } else {
+        int mid = (start + end) / 2;
+        if (i <= mid) {
+            segment_tree_update(a, tree, start, mid, 2 * node + 1, i, value);
+        } else {
+            segment_tree_update(a, tree, mid + 1, end, 2 * node + 2, i, value);
+        }
+        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+    }
+}
+
+int segemnt_tree_query(vector<int> &tree, int start, int end, int node, int l, int r) {
+    if (l > end || r > start) {
+        return 0;
+    } else if (start >= l && end <= r) {
+        return tree[node];
+    } else {
+        int mid = (start + end) / 2;
+        int p1 = segemnt_tree_query(tree, start, mid, 2 * node + 1, l , r);
+        int p1 = segemnt_tree_query(tree, mid + 1, end, 2 * node + 2, l , r);
+        return (p1 + p2);
+    }
+}
