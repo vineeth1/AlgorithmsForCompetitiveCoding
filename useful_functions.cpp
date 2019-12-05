c82c1fa96521f4b0b3f6355a5df80be3abe85118
Vineeth <vineeththota1315268@gmail.com>
2019-12-06 00:08:14 +0530

    Update useful_functions.cpp

--- useful_functions.cpp
+++ useful_functions.cpp
@@ -2,13 +2,13 @@
 
 int gcd(int a,int b){return b?gcd(b,a%b):a;}
 
-void build_segement_tree(vector<int> &a, vector<int> &tree, int start, int end, int node) {
+void build_segment_tree(vector<int> &a, vector<int> &tree, int start, int end, int node) {
     if (start == end) {
         tree[node] = a[start];
     } else {
         int mid = (start + end) / 2;
-        build_segement_tree(a, tree, start, mid, 2 * node + 1);
-        build_segement_tree(a, tree, mid + 1, end, 2 * node + 2);
+        build_segment_tree(a, tree, start, mid, 2 * node + 1);
+        build_segment_tree(a, tree, mid + 1, end, 2 * node + 2);
         tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
     }
 }
@@ -29,14 +29,14 @@ void segment_tree_update(vector<int> &a, vector<int> &tree, int start, int end, 
 }
 
 int segment_tree_query(vector<int> &tree, int start, int end, int node, int l, int r) {
-    if (l > end || r > start) {
+    if (l > end || r < start) {
         return 0;
     } else if (start >= l && end <= r) {
         return tree[node];
     } else {
         int mid = (start + end) / 2;
-        int p1 = segemnt_tree_query(tree, start, mid, 2 * node + 1, l , r);
-        int p1 = segemnt_tree_query(tree, mid + 1, end, 2 * node + 2, l , r);
+        int p1 = segment_tree_query(tree, start, mid, 2 * node + 1, l , r);
+        int p2 = segment_tree_query(tree, mid + 1, end, 2 * node + 2, l , r);
         return (p1 + p2);
     }
 }
